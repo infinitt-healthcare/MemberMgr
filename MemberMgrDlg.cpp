d6cbf06c30cbd0ec6edec53fd596af2e5763deec
dongdonghuk <jay5713665@gmail.com>
2022-04-19 12:17:53 +0900

    초기 커밋.

---
+++ MemberMgrDlg.cpp
@@ -0,0 +1,237 @@
+﻿
+// MemberMgrDlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MemberMgr.h"
+#include "MemberMgrDlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButtonNew();
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON_NEW, &CAboutDlg::OnBnClickedButtonNew)
+END_MESSAGE_MAP()
+
+
+// CMemberMgrDlg 대화 상자
+
+
+
+CMemberMgrDlg::CMemberMgrDlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MEMBERMGR_DIALOG, pParent)
+	, m_strID(_T(""))
+	, m_strName(_T(""))
+	, m_intPostCode(0)
+	, m_strAdress(_T(""))
+	, m_bMale(FALSE)
+	, m_bFemale(FALSE)
+	, m_strPhone1(_T(""))
+	, m_strPhone2(_T(""))
+	, m_strPhone3(_T(""))
+	, m_intAge(0)
+	, m_iHobby1(0)
+
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMemberMgrDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_ID, m_strID);
+	DDX_Text(pDX, IDC_EDIT_NAME, m_strName);
+	DDX_Text(pDX, IDC_EDIT_POST_CODE, m_intPostCode);
+	DDX_Text(pDX, IDC_EDIT_ADRESS, m_strAdress);
+	DDX_Check(pDX, IDC_CHECK_MALE, m_bMale);
+	DDX_Check(pDX, IDC_CHECK_FEMALE, m_bFemale);
+	DDX_Text(pDX, IDC_EDIT_PHONE1, m_strPhone1);
+	DDX_Text(pDX, IDC_EDIT_PHONE2, m_strPhone2);
+	DDX_Text(pDX, IDC_EDIT_PHONE3, m_strPhone3);
+	DDX_Text(pDX, IDC_EDIT_AGE, m_intAge);
+	DDX_Radio(pDX, IDC_RADIO_HOBBY1, m_iHobby1);
+
+}
+
+BEGIN_MESSAGE_MAP(CMemberMgrDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_CHECK_MALE, &CMemberMgrDlg::OnBnClickedCheckMale)
+	ON_BN_CLICKED(IDC_RADIO_HOBBY1, &CMemberMgrDlg::OnBnClickedRadioHobby1)
+	ON_BN_CLICKED(IDOK, &CMemberMgrDlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDC_BUTTON_NEW, &CMemberMgrDlg::OnBnClickedButtonNew)
+END_MESSAGE_MAP()
+
+
+// CMemberMgrDlg 메시지 처리기
+
+BOOL CMemberMgrDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMemberMgrDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMemberMgrDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMemberMgrDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMemberMgrDlg::OnBnClickedCheckMale()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
+
+
+void CMemberMgrDlg::OnBnClickedRadioHobby1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+
+	//CString strHobby = _T("");
+
+	switch (m_iHobby1)
+	{
+	case 0:
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+	}
+
+}
+
+
+void CMemberMgrDlg::OnBnClickedOk()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CDialogEx::OnOK();
+}
+
+
+void CMemberMgrDlg::OnBnClickedButtonNew()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+
+}
+
+
+void CAboutDlg::OnBnClickedButtonNew()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ MemberMgrDlg.h
@@ -0,0 +1,57 @@
+﻿
+// MemberMgrDlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMemberMgrDlg 대화 상자
+class CMemberMgrDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMemberMgrDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MEMBERMGR_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	// 아이디
+	CString m_strID;
+	// 이름
+	CString m_strName;
+	// 우편번호
+	int m_intPostCode;
+	// 주소
+	CString m_strAdress;
+	afx_msg void OnBnClickedCheckMale();
+	BOOL m_bMale;
+	BOOL m_bFemale;
+	// 전화번호
+	CString m_strPhone1;
+	CString m_strPhone2;
+	CString m_strPhone3;
+	// 나이
+	int m_intAge;
+	int m_iHobby1;
+
+	afx_msg void OnBnClickedRadioHobby1();
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedButtonNew();
+};
